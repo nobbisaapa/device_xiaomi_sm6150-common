33c6c66b40c2e5990728884144385e780502b72c
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2022-05-03 16:36:45 +0700

    sm6150-common: Remove some Gapps packages at build time
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,9 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Stk Drive GoogleTTS LocationHistoryPrebuilt MarkupGoogle Photos FilesPrebuilt
-LOCAL_OVERRIDES_PACKAGES += PrebuiltGmail talkback Videos YouTube YouTubeMusicPrebuilt DeskClock GoogleRestorePrebuilt
-LOCAL_OVERRIDES_PACKAGES += TurboPrebuilt Velvet WellbeingPrebuilt DevicePersonalizationPrebuiltPixel2021
+LOCAL_OVERRIDES_PACKAGES := Stk Drive GoogleTTS Photos
+LOCAL_OVERRIDES_PACKAGES += PrebuiltGmail YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES += Videos
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
